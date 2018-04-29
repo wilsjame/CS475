cca529dd601b36824667810406072775310919ad
James Wilson <wilsjame@oregonstate.edu>
2018-04-28 20:00:46 -0700

    update benchmarks script for project 3

---
+++ project2/benchmarks.sh
@@ -0,0 +1,12 @@
+#!/bin/csh
+
+echo "*** Project 3 Benchmarks ***"
+
+# number of threads:
+foreach t ( 1 2 4 )
+	# echo NUMTHREADS = $t
+	g++ -DNUMTHREADS=$t proj2.cpp -o proj2 -lm -fopenmp
+	./proj2
+end
+
+echo "*** Done ***"
