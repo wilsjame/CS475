9c9691d35bb104866a51b4ec74fe8c10f3400626
James Wilson <wilsjame@oregonstate.edu>
2018-04-29 16:52:57 -0700

    output formatting

--- project2/benchmarks.sh
+++ project2/benchmarks.sh
@@ -4,6 +4,7 @@ echo "*** Project 3 Benchmarks ***"
 
 # number of threads:
 foreach t ( 1 2 4 )
+	echo "---------------------------"
 	echo NUMTHREADS = $t
 	g++ -DNUMTHREADS=$t proj2.cpp -o proj2 -lm -fopenmp
 	./proj2
