876be6de483b4b8455bee772366a6812755b2663
James Wilson <wilsjame@oregonstate.edu>
2018-04-29 16:48:52 -0700

    print number of threads

--- project2/benchmarks.sh
+++ project2/benchmarks.sh
@@ -4,7 +4,7 @@ echo "*** Project 3 Benchmarks ***"
 
 # number of threads:
 foreach t ( 1 2 4 )
-	# echo NUMTHREADS = $t
+	echo NUMTHREADS = $t
 	g++ -DNUMTHREADS=$t proj2.cpp -o proj2 -lm -fopenmp
 	./proj2
 end
