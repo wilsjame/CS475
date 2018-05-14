b121a4ba0905dfa2e53e9c80942d0931a045b72f
James Wilson <wilsjame@oregonstate.edu>
2018-05-14 16:48:27 -0700

    add print state of Grainville function

--- project4/grainville.cpp
+++ project4/grainville.cpp
@@ -10,7 +10,7 @@
 ** to eat it. The number of graindeer depends on the amount of grain 
 ** available to eat. 
 *********************************************************************/
-#include <stdio.h>
+#include <iostream>
 #include <omp.h>
 #include <math.h>
 #include <stdlib.h>
@@ -23,6 +23,7 @@ void Watcher();
 
 // Utility functions. 
 void computeWeather(); 
+void printState();
 float SQR(float);
 float Ranf(unsigned int*, float, float);
 int Randf(unsigned int*, int, int);
@@ -67,6 +68,8 @@ int main()
 	// Starting temperature and precipitation.
 	computeWeather();
 	
+	printState();
+	
 	// Know how many threads already, start them with parallel sections
 	omp_set_num_threads( 3 );	// same as # of sections
 	#pragma omp parallel sections
@@ -98,25 +101,6 @@ int main()
 	return 0;
 }
 
-// Calculate temperature and precipitation.
-void computeWeather()
-{
-	
-	float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
-
-	float temp = AVG_TEMP - AMP_TEMP * cos( ang );
-	unsigned int seed = 0;
-	NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
-	
-	float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
-	NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
-	if( NowPrecip < 0. )
-	{
-		NowPrecip = 0.;
-	}
-	
-}
-
 // Compute next number of graindeer, based on set of global variables,
 // into local, temporary, variables. 
 void GrainDeer()
@@ -145,10 +129,6 @@ void GrainDeer()
 		// DoneAssigning barrier:
 		#pragma omp barrier
 		
-		// Print current set of global state variables. 
-		// Increment month count.
-		// Compute new temperature and precipitation. 
-	
 		// DonePrinting barrier:
 		#pragma omp barrier
 		//. . .
@@ -203,17 +183,18 @@ void Watcher()
 	
 		while( NowYear < 2023 )
 	{
-		// compute a temporary next-value for this quantity
-		// based on the current state of the simulation:
-		// NULL
 	
 		// DoneComputing barrier:
 		#pragma omp barrier
-		// NULL
-	
+
 		// DoneAssigning barrier:
 		#pragma omp barrier
 		
+		// Print current set of global state variables. 
+		printState();
+		// Increment month count.
+		// Compute new temperature and precipitation. 
+	
 	
 		// DonePrinting barrier:
 		#pragma omp barrier
@@ -224,6 +205,66 @@ void Watcher()
 
 //void MyAgent();
 
+// Calculate temperature and precipitation.
+void computeWeather()
+{
+	
+	float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
+
+	float temp = AVG_TEMP - AMP_TEMP * cos( ang );
+	unsigned int seed = 0;
+	NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
+	
+	float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
+	NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
+	if( NowPrecip < 0. )
+	{
+		NowPrecip = 0.;
+	}
+	
+}
+
+/*
+// The "state" of the system consists of the following global variables.
+int	NowYear;			// 2017 - 2022
+int	NowMonth;		// 0 - 11
+
+float	NowPrecip;		// inches of rain per month
+float	NowTemp;			// temperature this month
+float	NowHeight;		// grain height in inches
+int	NowNumDeer;		// number of deer in the current population
+
+// Initialize temperature and precipitation constants. 
+const float GRAIN_GROWS_PER_MONTH =		8.0;	// inches
+const float ONE_DEER_EATS_PER_MONTH =	0.5;
+
+const float AVG_PRECIP_PER_MONTH =		6.0;	// average (inches)
+const float AMP_PRECIP_PER_MONTH =		6.0;	// plus or minus
+const float RANDOM_PRECIP =				2.0;	// plus or minus noise
+
+const float AVG_TEMP =						50.0;	// average (deg_F)
+const float AMP_TEMP =						20.0;	// plus or minus
+const float RANDOM_TEMP =					10.0;	// plus or minus noise
+
+const float MIDTEMP =						40.0;
+const float MIDPRECIP =						10.0;
+*/
+
+// Print current set of global state variables. 
+void printState()
+{
+	std::cout << "Temperature (F): " << NowTemp << "\n";
+	std::cout << "Temperature (C): " << (5./9.)*(NowTemp-32) << "\n";
+	std::cout << "Precipitation (in): " << NowPrecip << "\n";
+	std::cout << "Precipitation (cm): " << NowPrecip*2.54 << "\n";
+	std::cout << "Number of graindeer: " << NowNumDeer << "\n";
+	std::cout << "Height of grain (in): " << NowHeight << "\n";
+	std::cout << "Height of grain (cm): " << NowHeight*2.54 << "\n";
+	std::cout << "Own quantity: " << "NULL" << "\n";
+	std::cout << "Year #: " << NowYear << "\n";
+	std::cout << "Month #: " << NowMonth << "\n";
+}
+// Utility function.
 float SQR(float x)
 {
 	
@@ -231,6 +272,7 @@ float SQR(float x)
 
 }
 
+// Random utility function. 
 float
 Ranf( unsigned int *seedp,  float low, float high )
 {
@@ -238,7 +280,6 @@ Ranf( unsigned int *seedp,  float low, float high )
 
         return(   low  +  r * ( high - low ) / (float)RAND_MAX   );
 }
-
 
 int
 Ranf( unsigned int *seedp, int ilow, int ihigh )
